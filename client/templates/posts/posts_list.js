930ab7d2b8dc742124126f4cfc63b0dcc0f340d6
Natalia Fierro <nataliabfierro@gmail.com>
2015-02-10 17:27:46 -0800

    wired collection into postsList template

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,22 +1,9 @@
-var postsData = [
-  {
-    title: 'Introducing Telescope',
-    url: 'http://sachagreif.com/introducing-telescope/'
-  },
-  {
-    title: 'Meteor',
-    url: 'http://meteor.com'
-  },
-  {
-    title: 'The Meteor Book',
-    url: 'http://themeteorbook.com'
-  }
-];
-
-//function to create a template helper called posts that returns the postsData array we just defined above
+//function to create a template helper called posts that returns the Posts collection
 Template.postsList.helpers({
-  posts: postsData
+  posts: function() {
+    return Posts.find();
+  }
 });
 
 
-//Defining the posts helper means it is now available for our template to use, so our template will be able to iterate over our postsData array and pass each object contained within to the postItem template.
+//Defining the posts helper means it is now available for our template to use.
