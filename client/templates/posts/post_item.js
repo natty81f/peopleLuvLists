f6558c4600315463ce06b048c3264972d9a0824c
Natalia Fierro <nataliabfierro@gmail.com>
2015-02-18 18:01:26 -0800

    added edit posts form

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -1,4 +1,9 @@
 Template.postItem.helpers({
+  //don't show you an edit link to somebody else's form
+  ownPost: function() {
+    return this.userId === Meteor.userId();
+  },
+
   domain: function() {
     var a = document.createElement('a');
     a.href = this.url;
