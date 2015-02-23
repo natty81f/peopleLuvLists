72317e7ae9e55ac1eaa95baa4147457eac7af616
Natalia Fierro <nataliabfierro@gmail.com>
2015-02-23 15:07:17 -0800

    Clear errors after 3 seconds

--- client/templates/includes/errors.js
+++ client/templates/includes/errors.js
@@ -3,3 +3,12 @@ Template.errors.helpers({
     return Errors.find();
   }
 });
+
+//Clear errors after 3 seconds.
+//Use Meteor.setTimeout to specify a callback function to be executed after the timeout (in this case, 3000 milliseconds) expires
+Template.error.rendered = function() {
+  var error = this.data;
+  Meteor.setTimeout(function () {
+    Errors.remove(error._id);
+  }, 3000);
+};
