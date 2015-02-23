0ffbd4fa16cec26a015a3e62d09c739c9bde3829
Natalia Fierro <nataliabfierro@gmail.com>
2015-02-23 15:42:39 -0800

    validate post contents on submission

--- client/templates/includes/errors.js
+++ client/templates/includes/errors.js
@@ -5,7 +5,8 @@ Template.errors.helpers({
 });
 
 //Clear errors after 3 seconds.
-//Use Meteor.setTimeout to specify a callback function to be executed after the timeout (in this case, 3000 milliseconds) expires
+//Use Meteor.setTimeout to specify a callback function to be executed after the timeout (in this case, 3000 milliseconds) expires.
+//The rendered callback triggers once our template has been rendered in the browser. Inside the callback, this refers to the current template instance, and this.data lets us access the data of the object that is currently being rendered (in our case, an error).
 Template.error.rendered = function() {
   var error = this.data;
   Meteor.setTimeout(function () {
--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -1,3 +1,20 @@
+//Initialize the object whenever the postSubmit template is created. This ensures that the user won't see old error messages left over from a previous visit to this page.
+//We then define our two template helpers. They both look at the field property of Session.get('postSubmitErrors') (where field is either url or title depending on where we're calling the helper from).
+//While errorMessage simply returns the message itself, errorClass checks for the presence of a message and returns has-error if such a message exists.
+Template.postSubmit.created = function() {
+  Session.set('postSubmitErrors', {});
+}
+Template.postSubmit.helpers({
+  errorMessage: function(field) {
+    return Session.get('postSubmitErrors')[field];
+  },
+  errorClass: function (field) {
+    return !!Session.get('postSubmitErrors')[field] ? 'has-error' : '';
+  }
+});
+
+
+
 Template.postSubmit.events({
   'submit form': function(e) {
     e.preventDefault();
@@ -6,6 +23,13 @@ Template.postSubmit.events({
       url: $(e.target).find('[name=url]').val(),
       title: $(e.target).find('[name=title]').val()
     };
+
+
+    //Calling the validatePost function
+    var errors = validatePost(post);
+    if (errors.title || errors.url)
+      return Session.set('postSubmitErrors', errors);
+
 
     Meteor.call('postInsert', post, function(error, result) {
       // display the error to the user and abort
