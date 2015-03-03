73c37d2cf1129e96809475c225301aa79e56ba61
Natalia Fierro <nataliabfierro@gmail.com>
2015-03-03 13:28:30 -0800

    made a simple publication/subscription for comments

--- server/publications.js
+++ server/publications.js
@@ -3,6 +3,7 @@ Meteor.publish('posts', function() {
   return Posts.find();
 });
 
-Meteor.publish('comments', function() {
-  return Comments.find();
-});
+//Making sure we restrict our data set to comments belonging to the current post:
+Meteor.publish('comments', function(postId) {
+  check(postId, String);
+  return Comments.find({postId: postId});
