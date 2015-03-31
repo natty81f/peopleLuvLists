5ca724433bb97924a63d8c07bb1cb45c3a63940a
Natalia Fierro <nataliabfierro@gmail.com>
2015-03-31 11:22:39 -0700

    Added post reordering animation

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,11 +1,39 @@
-//function to create a template helper called posts that returns the Posts collection
-//Defining the posts helper means it is now available for our template to use
-// Template.postsList.helpers({
-//   posts: function() {
-//     return Posts.find({}, {sort: {submitted: -1}});
-//   }
-// });
+Template.postsList.rendered = function () {
+  this.find('.wrapper')._uihooks = {
+  	//The moveElement function we just defined will be called whenever an element's position changes
+    moveElement: function (node, next) {
+       var $node = $(node), $next = $(next);
+      var oldTop = $node.offset().top;
+      var height = $node.outerHeight(true);
 
-//*since we're setting the data context at the route level, we can now safely get rid of the posts template
+      // find all the elements between next and node
+      var $inBetween = $next.nextUntil(node);
+      if ($inBetween.length === 0)
+        $inBetween = $node.nextUntil(next);
+
+      // now put node in place
+      $node.insertBefore(next);
+
+      // measure new top
+      var newTop = $node.offset().top;
+
+      // move node *back* to where it was before
+      $node
+        .removeClass('animate')
+        .css('top', oldTop - newTop);
+
+      // push every other element down (or up) to put them back
+      $inBetween
+        .removeClass('animate')
+        .css('top', oldTop < newTop ? height : -1 * height)
 
 
+      // force a redraw
+      $node.offset();
+
+      // reset everything to 0, animated
+      $node.addClass('animate').css('top', 0);
+      $inBetween.addClass('animate').css('top', 0);
+    }
+  }
+}
