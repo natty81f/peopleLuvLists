c2a365e98951808c53d61843e296088fc9fc148b
Natalia Fierro <nataliabfierro@gmail.com>
2015-04-02 15:24:49 -0700

    fade items in when they are drawn

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,8 +1,16 @@
 Template.postsList.rendered = function () {
   this.find('.wrapper')._uihooks = {
+    //fades in new posts
+    insertElement: function (node, next) {
+      $(node)
+        .hide()
+        .insertBefore(next)
+        .fadeIn();
+    },
+
   	//The moveElement function we just defined will be called whenever an element's position changes
     moveElement: function (node, next) {
-       var $node = $(node), $next = $(next);
+      var $node = $(node), $next = $(next);
       var oldTop = $node.offset().top;
       var height = $node.outerHeight(true);
 
@@ -34,6 +42,13 @@ Template.postsList.rendered = function () {
       // reset everything to 0, animated
       $node.addClass('animate').css('top', 0);
       $inBetween.addClass('animate').css('top', 0);
+    },
+
+    //fades out removed posts
+    removeElement: function(node) {
+      $(node).fadeOut(function() {
+        $(this).remove();
+      });
     }
   }
 }
