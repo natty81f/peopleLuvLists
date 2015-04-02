ada80d0ad849085693bb348a2c5dead91ffb87c2
Natalia Fierro <nataliabfierro@gmail.com>
2015-04-02 15:34:48 -0700

    Transition between pages by fading

---
+++ client/templates/application/layout.js
@@ -0,0 +1,16 @@
+//animated transitions in-between pages
+Template.layout.rendered = function() {
+  this.find('#main')._uihooks = {
+    insertElement: function(node, next) {
+      $(node)
+        .hide()
+        .insertBefore(next)
+        .fadeIn();
+    },
+    removeElement: function(node) {
+      $(node).fadeOut(function() {
+        $(this).remove();
+      });
+    }
+  }
+}
